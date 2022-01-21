a11e3c67c9ddbcdf591018abef30d73ce589164b
JohnDuranM <91157723+JohnDuranM@users.noreply.github.com>
2022-01-21 17:06:41 -0500

    Mejora botones copiar - pegar
    Se mejora funcionalidad botón copiar y se agrega función  botón pegar

---
+++ js/button-copy-paste.js
@@ -0,0 +1,18 @@
+var buttonCopy = document.querySelector("#button-copy");
+var buttonPaste = document.querySelector("#button-paste");
+
+buttonCopy.addEventListener("click", function(event) {
+    event.preventDefault();
+    var text = document.getElementById("outputTexto");
+    text.select();
+    document.execCommand('copy');
+});
+
+buttonPaste.addEventListener("click", function(event) {
+    console.log("clic Paste");
+    event.preventDefault();
+
+    //paste 
+    var text = document.getElementById("outputTexto");
+    document.getElementById("inputTexto").value = text.value.toString();
+});
