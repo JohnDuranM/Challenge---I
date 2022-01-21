fde198a93ecb93d0bd1e48c0ecee02ca10d80da4
JohnDuranM <91157723+JohnDuranM@users.noreply.github.com>
2022-01-21 17:10:00 -0500

    Mejora en button encrypt-decrypt
    se mejora la performance de las funcionalidades encrypt - decrypt

--- js/encrypt-decrypt.js
+++ js/encrypt-decrypt.js
@@ -21,11 +21,12 @@ buttonDecrypt.addEventListener("click", function(event) {
 function encryptText(formInput) {
     text = inputTexto.value.toLowerCase();
 
-    text = text.replace("e", "enter");
-    text = text.replace("i", "imes");
-    text = text.replace("a", "ai");
-    text = text.replace("o", "ober");
-    text = text.replace("u", "ufat");
+    text = text.replace(new RegExp("e",'g'), "enter");
+    text = text.replace(new RegExp("i",'g'), "imes");
+    text = text.replace(new RegExp("a",'g'), "ai");
+    text = text.replace(new RegExp("o",'g'), "ober");
+    text = text.replace(new RegExp("u",'g'), "ufat");
+    text = text.replace(new RegExp("[àáâãäå\\sæçèéêëìíîïñòóôõöœùúûüýÿ\\W1234567890]", 'g'),"");
 
     document.getElementById("outputTexto").value = text.toLowerCase();
     console.log(text);
@@ -34,12 +35,38 @@ function encryptText(formInput) {
 function decryptText(formInput) {
     text = inputTexto.value.toLowerCase();
 
-    text = text.replace("enter", "e")
-    text = text.replace("imes", "i")
-    text = text.replace("ai", "a")
-    text = text.replace("ober", "o")
-    text = text.replace("ufat", "u")
+    text = text.replace(new RegExp("enter",'g'), "e")
+    text = text.replace(new RegExp("imes",'g'), "i")
+    text = text.replace(new RegExp("ai",'g'), "a")
+    text = text.replace(new RegExp("ober",'g'), "o")
+    text = text.replace(new RegExp("ufat",'g'), "u")
+;
 
     document.getElementById("outputTexto").value = text.toLowerCase();
     console.log(text);
 }
+
+
+
+/* const removeAccents = (str) => {
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  } 
+
+-----
+
+accentsTidy = function(s){
+    var text=s.toLowerCase();
+    text = text.replace(new RegExp("\\s", 'g'),"");
+    text = text.replace(new RegExp("[àáâãäå]", 'g'),"a");
+    text = text.replace(new RegExp("æ", 'g'),"ae");
+    text = text.replace(new RegExp("ç", 'g'),"c");
+    text = text.replace(new RegExp("[èéêë]", 'g'),"e");
+    text = text.replace(new RegExp("[ìíîï]", 'g'),"i");
+    text = text.replace(new RegExp("ñ", 'g'),"n");                            
+    text = text.replace(new RegExp("[òóôõö]", 'g'),"o");
+    text = text.replace(new RegExp("œ", 'g'),"oe");
+    text = text.replace(new RegExp("[ùúûü]", 'g'),"u");
+    text = text.replace(new RegExp("[ýÿ]", 'g'),"y");
+    text = text.replace(new RegExp("\\W", 'g'),"");
+    return text;
+}; */
