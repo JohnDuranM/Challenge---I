26169103cd90918b85a84de9163a9816701f0e3a
JohnDuranM <91157723+JohnDuranM@users.noreply.github.com>
2022-01-21 17:08:19 -0500

    Delete button- copy.js
    Se elimina para agregar button copy-paste.js

--- js/button-copy.js
+++
@@ -1,9 +0,0 @@
-var buttonCopy = document.querySelector("#button-copy");
-
-buttonCopy.addEventListener("click", function(event) {
-    event.preventDefault();
-    var text = document.getElementById("outputTexto");
-    text.select();
-    document.execCommand('copy');
-    alert("tu texto ha sido copiado");
-});
