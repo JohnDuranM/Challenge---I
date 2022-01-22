3646592c0b1071912e54e54b105cf654c746427d
JohnDuranM <91157723+JohnDuranM@users.noreply.github.com>
2022-01-22 15:21:58 -0500

    Update paste button
    El boton pegar traslada texto al input y elimina el ouput.value del mensaje ya cifrado

--- js/button-copy-paste.js
+++ js/button-copy-paste.js
@@ -15,4 +15,5 @@ buttonPaste.addEventListener("click", function(event) {
     //paste 
     var text = document.getElementById("outputTexto");
     document.getElementById("inputTexto").value = text.value.toString();
+    document.getElementById("outputTexto").value = "";
 });
